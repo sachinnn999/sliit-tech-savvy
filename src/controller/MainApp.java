f1b774a81ecfaca0599120a382fdcb4cd908a565
Sachinnnn999 <sachin.rds99@gmail.com>
2021-09-20 02:25:01 +0530

    exception handled

--- src/controller/MainApp.java
+++ src/controller/MainApp.java
@@ -3,6 +3,7 @@ package controller;
 import exception.GameOverException;
 import exception.SoundException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MainApp {
@@ -16,14 +17,18 @@ public class MainApp {
         while (true){
             System.out.println("X: Y: pls?");
             scanner = new Scanner(System.in);
-            int x = scanner.nextInt();
-            scanner = new Scanner(System.in);
-            int y = scanner.nextInt();
             try {
+                int x = scanner.nextInt();
+                scanner = new Scanner(System.in);
+                int y = scanner.nextInt();
                 myBoard.moveHunter(x, y);
             }catch (SoundException ignored){
             }catch (GameOverException go){
                 break;
+            }catch (InputMismatchException e){
+                System.out.println("Numbers are only accepted!!");
+            }catch (Exception e){
+                System.out.println("Something went wrong! Try again Later!");
             }
         }
     }
